256acb5d795de6b5f27b5f9ed3dc231a7d7a93f3
PabloJVelez <pmltechpile@gmail.com>
2025-08-20 10:47:59 -0500

    Revert "fix: testing changes for vercel deployment"
    This reverts commit c54a46d565aadc17eb7224dbec57c07e7cd9c61d.

--- apps/storefront/api/index.js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from "@react-router/node";
-import * as build from "../build/server/index.js";
-
-const handler = createRequestHandler(build);
-
-export default function (req, res) {
-  return handler(req, res);
-} 
