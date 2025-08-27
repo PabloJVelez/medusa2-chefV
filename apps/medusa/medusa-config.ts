14753c9834b056f6736d01c266a383c126a8cf58
PabloJVelez <pmltechpile@gmail.com>
2025-08-26 19:31:31 -0500

    feat: s3 file storage support

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -64,6 +64,27 @@ const notificationModule = {
       },
     };
 
+const fileModule = {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              // other options...
+            },
+          },
+        ],
+      },
+    };
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -110,6 +131,7 @@ module.exports = defineConfig({
     eventBusModule,
     workflowEngineModule,
     notificationModule,
+    fileModule,
   ],
   // admin: {
   //   // ADD ADMIN DISABLE CONFIGURATION
