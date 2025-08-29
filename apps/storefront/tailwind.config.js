3510efd12dfd3f91715e096c58ba8a66e03689cd
PabloJVelez <pmltechpile@gmail.com>
2025-08-28 22:36:57 -0500

    config cleanup

--- apps/storefront/tailwind.config.js
+++ apps/storefront/tailwind.config.js
@@ -66,6 +66,11 @@ module.exports = {
       },
       screens: {
         xs: '400px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
       fontSize: {
         '2xs': '.65rem',
